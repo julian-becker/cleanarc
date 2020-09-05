ef2da96272a8b27eb814c19a0dea3d2040f134b2
Julian Becker <becker.julian@gmail.com>
2020-09-05 23:47:20 +0200

    chore: modified metro resolution in order to avoid warning during Chrome-debug session

--- app/App.tsx
+++ app/App.tsx
@@ -1,70 +1,13 @@
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar
-} from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen';
-import CppModule1 from '@workspace/react-native-cpp-module1';
-
-declare const global: {HermesInternal: null | {}};
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  useEffect(() => {
-    CppModule1.multiply(10, 5);
-  }, []);
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discoer what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Test12</Text>
+      </View>
     </>
   );
 };
--- app/metro.config.js
+++ app/metro.config.js
@@ -1,52 +1,64 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 const path = require('path');
-
 const {FileStore} = require('metro-cache');
-
+const {resolve} = require('metro-resolver');
 const getWorkspaces = require('get-yarn-workspaces');
-const blacklist = require('metro-config/src/defaults/blacklist');
-
 const workspaces = getWorkspaces(__dirname);
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   projectRoot: path.resolve(__dirname, '.'),
-
   watchFolders: [path.resolve(__dirname, '../node_modules'), ...workspaces],
 
   resolver: {
-    blacklistRE: blacklist(
-      workspaces
-        .filter((workspacePath) => workspacePath.indexOf('app') === -1)
-        .map(
-          (workspacePath) =>
-            new RegExp(
-              `^${escape(
-                path.resolve(__dirname, workspacePath, 'node_modules')
-              )}\\/.*$`
-            )
-        )
-    ),
-    // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/
+    ]),
+
     extraNodeModules: new Proxy(
       {},
       {
         get: (target, name) => path.join(process.cwd(), `node_modules/${name}`)
       }
-    )
+    ),
+
+    resolveRequest: (context, realModuleName, platform, moduleName) => {
+      let backupResolveRequest = context.resolveRequest;
+      delete context.resolveRequest;
+
+      try {
+        let modifiedModuleName = moduleName;
+        if (moduleName.startsWith('./debugger-ui/')) {
+          modifiedModuleName = `./node_modules/@react-native-community/cli-debugger-ui/build/ui/${modifiedModuleName.slice(
+            './debugger-ui/'.length
+          )}`;
+        }
+        let result = resolve(context, modifiedModuleName, platform);
+        return result;
+      } catch (e) {
+        throw e;
+      } finally {
+        context.resolveRequest = backupResolveRequest;
+      }
+    }
   },
 
-  // http://facebook.github.io/react-native/blog/2019/03/12/releasing-react-native-059#faster-app-launches-with-inline-requires
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: false
       }
-    })
-  },
-
-  cacheStores: [
-    new FileStore({
-      root: path.join(__dirname, 'metro-cache')
-    })
-  ]
+    }),
+    cacheStores: [
+      new FileStore({
+        root: path.join(__dirname, 'metro-cache')
+      })
+    ]
+  }
 };
