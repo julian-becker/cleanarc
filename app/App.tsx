72f33885cd8b29022729fd56c7315feaeeabedd7
Julian Becker <becker.julian@gmail.com>
2020-09-07 21:21:36 +0200

    chore: hoist most packages, except react-native things that would break

--- app/App.tsx
+++ app/App.tsx
@@ -6,23 +6,13 @@ const App = () => {
   return (
     <>
       <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Test12</Text>
+        <Text style={styles.sectionTitle}>Test</Text>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: 'absolute',
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24
@@ -31,23 +21,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: '700'
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right'
   }
 });
 
